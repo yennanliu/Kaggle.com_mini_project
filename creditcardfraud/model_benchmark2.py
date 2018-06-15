9bd9d8638e15795e59b6d5118826e6d2d0173bf4
yennanliue <f339339@gmail.com>
2018-06-15 17:44:35 +0100

    add get best c param logisticregression function

--- creditcardfraud/model_benchmark2.py
+++ creditcardfraud/model_benchmark2.py
@@ -9,6 +9,9 @@ import os
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import average_precision_score, confusion_matrix
 from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import KFold, cross_val_score
+from sklearn.metrics import confusion_matrix,precision_recall_curve,auc,roc_auc_score,roc_curve,recall_score,classification_report 
 
 
 np.random.seed(7)
@@ -21,7 +24,7 @@ def get_data():
 	print (df.head())
 	print (df.columns)
 	X = df.ix[:, df.columns != 'Class']
-	y = df.ix[:, df.columns != 'Class']
+	y = df.ix[:, df.columns == 'Class']
 	return df, X, y 
 
 
@@ -38,7 +41,7 @@ def get_under_sample_train_test_data(df):
 	# get the index of data labeled as fraud (class = 1 )
 	fraud_indices = np.array(df[df.Class == 1].index)
 	# get the index of data labeled as fraud (class = 1 )
-	normal_indices = np.array(df[df.Class == 1].index)
+	normal_indices = np.array(df[df.Class == 0].index)
 	# random select the data  
 	###### can modify here ######
 	random_normal_indices = np.random.choice(normal_indices, num_data_fraud, replace = False)
@@ -57,6 +60,62 @@ def get_under_sample_train_test_data(df):
 
 
 #-----------------------------------
+# ML 
+
+def get_best_param_Kfold(x_train_data,y_train_data):
+    fold = KFold(len(y_train_data),5,shuffle=False) 
+
+    # Different C parameters
+    c_param_range = [0.01,0.1,1,10,100]
+
+    results_table = pd.DataFrame(index = range(len(c_param_range),2), columns = ['C_parameter','Mean recall score'])
+    results_table['C_parameter'] = c_param_range
+
+    # the k-fold will give 2 lists: train_indices = indices[0], test_indices = indices[1]
+    j = 0
+    for c_param in c_param_range:
+        print('-------------------------------------------')
+        print('C parameter: ', c_param)
+        print('-------------------------------------------')
+        print('')
+
+        recall_accs = []
+        for iteration, indices in enumerate(fold,start=1):
+
+            # Call the logistic regression model with a certain C parameter
+            lr = LogisticRegression(C = c_param, penalty = 'l1')
+
+            # Use the training data to fit the model. In this case, we use the portion of the fold to train the model
+            # with indices[0]. We then predict on the portion assigned as the 'test cross validation' with indices[1]
+            lr.fit(x_train_data.iloc[indices[0],:],y_train_data.iloc[indices[0],:].values.ravel())
+
+            # Predict values using the test indices in the training data
+            y_pred_undersample = lr.predict(x_train_data.iloc[indices[1],:].values)
+
+            # Calculate the recall score and append it to a list for recall scores representing the current c_parameter
+            recall_acc = recall_score(y_train_data.iloc[indices[1],:].values,y_pred_undersample)
+            recall_accs.append(recall_acc)
+            print('Iteration ', iteration,': recall score = ', recall_acc)
+
+        # The mean value of those recall scores is the metric we want to save and get hold of.
+        results_table.ix[j,'Mean recall score'] = np.mean(recall_accs)
+        j += 1
+        print('')
+        print('Mean recall score ', np.mean(recall_accs))
+        print('')
+    # fix here :  .idxmax() -> .values.argmax()
+    # https://github.com/pandas-dev/pandas/issues/18021
+    best_c = results_table.loc[results_table['Mean recall score'].values.argmax()]['C_parameter']
+    
+    # Finally, we can check which C parameter is the best amongst the chosen.
+    print('*********************************************************************************')
+    print('Best model to choose from cross validation is with C parameter = ', best_c)
+    print('*********************************************************************************')
+    
+    return best_c
+
+#-----------------------------------
+
 
 
 if __name__ == '__main__':
@@ -65,11 +124,18 @@ if __name__ == '__main__':
 	print (' y :', y)
 	# preprocess data 
 	under_sample_data, X_undersample, y_undersample = get_under_sample_train_test_data(df)
-	#print (under_sample_data.head())
-	#print (X_undersample.head())
 	# get whole X, y data 
 	X_train, X_test, y_train, y_test = get_train_test_data(X,y)
 	# get undersample X, y data 
-	X_train_undersample, X_test_undersample, y_train_undersample, y_test_undersample = get_train_test_data( X_undersample, y_undersample)
+	X_train_undersample, X_test_undersample, y_train_undersample, y_test_undersample = get_train_test_data(X_undersample, y_undersample)
+	# get best super-parameter in logicregression model 
+	c_best = get_best_param_Kfold(X_train_undersample,y_train_undersample)
+	print (c_best)
+
+
+
+
+
+
 
 
