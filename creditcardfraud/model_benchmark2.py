50e9653fc4cc1dfe68f24a37428f8eba150b52cc
yennanliue <f339339@gmail.com>
2018-06-16 07:37:10 +0100

    print  confusion matrix

--- creditcardfraud/model_benchmark2.py
+++ creditcardfraud/model_benchmark2.py
@@ -12,6 +12,8 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import KFold, cross_val_score
 from sklearn.metrics import confusion_matrix,precision_recall_curve,auc,roc_auc_score,roc_curve,recall_score,classification_report 
+import itertools
+import matplotlib.pylab as plt
 
 
 np.random.seed(7)
@@ -57,6 +59,41 @@ def get_under_sample_train_test_data(df):
 	print ('under sample data : ', len(under_sample_data) )
 	return under_sample_data, X_undersample, y_undersample
 
+
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=0)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        #print("Normalized confusion matrix")
+    else:
+        1#print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
 
 
 #-----------------------------------
@@ -131,8 +168,28 @@ if __name__ == '__main__':
 	# get best super-parameter in logicregression model 
 	c_best = get_best_param_Kfold(X_train_undersample,y_train_undersample)
 	print (c_best)
+	# plot confusion matrix 
+	# Use this C_parameter to build the final model with the whole training dataset and predict the classes in the test
+	# dataset
+	lr = LogisticRegression(C = c_best, penalty = 'l1')
+	lr.fit(X_train_undersample,y_train_undersample.values.ravel())
+	y_pred_undersample = lr.predict(X_test_undersample.values)
 
+	# Compute confusion matrix
+	cnf_matrix = confusion_matrix(y_test_undersample,y_pred_undersample)
+	print ('--------------- cnf_matrix :  ---------------')
+	print (cnf_matrix)
+	np.set_printoptions(precision=2)
 
+	print("Recall metric in the testing dataset: ", cnf_matrix[1,1]/(cnf_matrix[1,0]+cnf_matrix[1,1]))
+
+	# Plot non-normalized confusion matrix
+	class_names = [0,1]
+	plt.figure()
+	plot_confusion_matrix(cnf_matrix
+	                      , classes=class_names
+	                      , title='Confusion matrix')
+	plt.show()
 
 
 
