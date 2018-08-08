e28697af528c0806f05f6ef5dc9a0a3aef691dc6
yennanliue <f339339@gmail.com>
2018-08-08 17:09:28 +0100

    add CNN model V3

--- SMS_spam_dataset/model_LSTM.py
+++ SMS_spam_dataset/model_LSTM.py
@@ -104,9 +104,6 @@ def CNN_model_V1():
 def CNN_model_V2(): 
     max_len=150
     model = Sequential()
-    # we start off with an efficient embedding layer which maps
-    # our vocab indices into embedding_dims dimensions
-    # 1000 is num_max
     model.add(Embedding(1000,
                         50, 
                         input_length=max_len))
@@ -127,6 +124,32 @@ def CNN_model_V2():
                   optimizer='adam',
                   metrics=['acc',metrics.binary_accuracy])
     return model
+
+def CNN_model_V3(): 
+    max_len=150  
+    model = Sequential()
+    model.add(Embedding(1000,
+                        20,
+                        input_length=max_len))
+    model.add(Dropout(0.2))
+    model.add(Conv1D(256, 
+                     3,
+                     padding='valid',
+                     activation='relu',
+                     strides=1))
+    model.add(GlobalMaxPooling1D())
+    model.add(Dense(256))
+    model.add(Dropout(0.2))
+    model.add(Activation('relu'))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+    model.summary()
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['acc',metrics.binary_accuracy])
+    return model
+
+
 
 
 # ---------------------------
@@ -156,13 +179,14 @@ def main():
 	print (' # ------------  compile the model and print its architecture  ------------ ')
 	# ----- model 1  -----
 	#model = LSTM_model()
-	#model.compile(loss='binary_crossentropy',optimizer=RMSprop(),metrics=['accuracy'])
 	# ----- model 2  -----
 	#model = LSTM_model_V2()
 	# ----- model 3  -----
 	#model = CNN_model_V1()
 	# ----- model 3  -----
 	model = CNN_model_V2()
+	# ----- model 4  -----
+	model = CNN_model_V3()
 	model.summary()
 	# train on train set 
 	print (' # ------------  train on train set   ------------ ')
